74b9b8251dfe8165d2c1b16cf73a4d71cc0754eb
PiotrCzerwinski <piotrek26061997@gmail.com>
2022-06-03 16:58:31 +0200

    Initial commit

---
+++ src/main/kotlin/pbs/pai/ala_pyszne/AlaPyszneApplication.kt
@@ -0,0 +1,11 @@
+package pbs.pai.ala_pyszne
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlaPyszneApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlaPyszneApplication>(*args)
+}
---
+++ src/test/kotlin/pbs/pai/ala_pyszne/AlaPyszneApplicationTests.kt
@@ -0,0 +1,13 @@
+package pbs.pai.ala_pyszne
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlaPyszneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
