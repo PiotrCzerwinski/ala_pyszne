c345ebd40b85adf06c317d8b32b9b37cf892d743
PiotrCzerwinski <piotrek26061997@gmail.com>
2022-06-25 11:18:21 +0200

    Added Dockerfile

--- src/test/kotlin/pbs/pai/ala_pyszne/AlaPyszneApplicationTests.kt
+++ src/test/kotlin/pbs/pai/ala_pyszne/AlaPyszneApplicationTests.kt
@@ -2,8 +2,10 @@ package pbs.pai.ala_pyszne
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("embedmongo")
 class AlaPyszneApplicationTests {
 
     @Test
